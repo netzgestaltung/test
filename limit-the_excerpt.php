494284edb7a5423a742dbb283f83b25017551d92
netzgestaltung <t.fellinger@netzgestaltung.at>
2014-06-26 19:06:55 +0200

    Create limit-the_excerpt.php
    limit the_excerpt() in wordpress

---
+++ limit-the_excerpt.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file limit the_excerpt()
+ *
+ * this should go into the functions.php of yourTheme.
+ * replace yourTheme with your Theme name.
+ *
+ * @see http://codex.wordpress.org/Function_Reference/the_excerpt#Control_Excerpt_Length_using_Filters
+ */
+function yourTheme_excerpt_length( $length ) {
+  // set words here
+	return 20;
+}
+
+add_filter( 'excerpt_length', 'yourTheme_excerpt_length', 999 );
+
+?>
